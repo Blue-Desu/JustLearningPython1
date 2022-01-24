dc9406d2addf7f3ca4613b0f5fca4440eafcbe52
Blue <purgekingsubaru@gmail.com]>
2022-01-24 12:36:56 +0530

    Initial commit

---
+++ Sayaka 1.py
@@ -0,0 +1,18 @@
+print("Hello World")
+print("sayaka" , " maizono")
+#Sayaka Maizono
+""""
+Kyoko Kirigiri
+Chihiro Fujisaki
+Celestia Ludenberg
+Sayaka Maizono
+"""
+
+print("am a " , end = " ")
+
+print("simp of sayaka")
+
+print("c:\\ansh")
+
+print("sayaka is best \n best girl ")
+
